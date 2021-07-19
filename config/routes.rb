5928d30b4f82ad7bc0a18a16fb4cd742fa453780
Mayowa Pitan <mayowa.pitan@andela.com>
2021-07-19 19:56:09 +0100

    configure controller and routes for devise-jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/api' do
-    devise_for :admin_users
+    devise_for :admin_users, defaults: { format: :json }
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
